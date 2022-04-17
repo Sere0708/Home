ffabada6f0ed44ede1dc22aa7f9e330a8365795c
Sergei <sere0708@mail.ru>
2022-04-17 23:11:52 +0300

    Home1

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+// Задача № 1  
+Console.WriteLine("Введите два числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+   Console.WriteLine("{0} наибольшее число", num1);
+}
+
+else
+
+{
+   Console.WriteLine("{0}) наибольшее число", num2);
+}
+Console.ReadLine();
+*/
+//. Задача № 2
+/*
+Console.Write("a = ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("b = ");
+double b = double.Parse(Console.ReadLine());
+Console.Write("c = ");
+double c = double.Parse(Console.ReadLine());
+if (a>b) a=b;
+if (a>c) a=c;
+if (b>c) b=c;
+Console.WriteLine("Max = {0}",a);
+*/
+
+//. Задача № 3.
+/*
+int i;
+Console.Write("Введите число: ");
+i = int.Parse(Console.ReadLine());
+if (i % 2 == 0)
+{
+    Console.Write("Введенное число является четным");
+    Console.Read();
+}
+else
+{
+    Console.Write("Введенное число нечетное");
+    Console.Read();
+}
+*/
+
+
+
+ 
+for (int x = 2; x < 101; x++)       
+if (x%2==0)            
+Console.WriteLine(x);
