c6e3386f79e3b20e2378ee5531ae9bfbda8f04e7
Sergei <sere0708@mail.ru>
2022-04-19 00:03:55 +0300

    Home2

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+// Задача 10.
+
+Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
+*/
+
+/*
+//. Задача. 13
+
+int n = 32679;
+        Console.WriteLine(n>99 ? n.ToString()[2] : '-');
+        Console.ReadKey(true);
+*/
+
+/*
+//. Задача 15.
+
+int day;
+
+day=int.Parse(Console.ReadLine());
+
+if(day<=5){
+
+Console.WriteLine("рабочий");
+
+}
+
+else{
+
+Console.WriteLine("выходной");
+
+}
+
+Console.ReadLine();
+*/
